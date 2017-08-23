07e50e83edfedde4b923c97c8873516376e8680f
robert-spidle <robert.spidle@ruralsourcing.com>
2017-08-23 13:16:31 -0600

    Updated custom filter methods to display a result total row for the filtered rows.

--- src/Results.js
+++ src/Results.js
@@ -14,18 +14,22 @@ class Results extends React.Component {
         this.state = {
             showDelete: false,
             data: mockData,
-            data2: mockData2
+            data2: mockData2,
+            displayData1: mockData,
+            displayData2: mockData2
         }
     
         this.delete = this.delete.bind(this);
         this.totalHourSum = this.totalHourSum.bind(this);
         this.setData = this.setData.bind(this);
+        this.setDisplayData = this.setDisplayData.bind(this);
 
     }
     
     componentDidMount(){
         this.totalHourSum(this.state.data);
     }
+    
 
     delete(name, email, date, hours) {
         console.log("Name: " + name + " Email: " + email + " Date: " + date + " Hours " + hours);
@@ -60,6 +64,18 @@ class Results extends React.Component {
         })
     }
 
+    setDisplayData(data){
+        if(this.props.displayScreen){
+            this.setState({
+                displayData1: data
+            })
+        } else {
+            this.setState({
+                displayData2: data
+            })
+        }
+    }
+
 
     render() {   
         const columns = [
@@ -72,8 +88,8 @@ class Results extends React.Component {
                     minWidth: 350,
                     resizeable: 'false',
                     filterable: 'true',
-                    filterMethod: (filter, row) =>
-                        String(row[filter.id]).toLowerCase().includes(String(filter.value).toLowerCase()),
+                    filterAll: 'true',
+                    
                     headerStyle: {textAlign: 'left', borderColor: 'black', borderStyle: 'none none outset none', borderWidth: '2px', marginLeft: '10px',},
                     style: {textAlign: 'left',borderColor: 'lightgrey', borderStyle: 'none none outset none',borderWidth: '1px', marginLeft: '10px'}
                 }, {
@@ -82,8 +98,7 @@ class Results extends React.Component {
                     minWidth: 350,
                     resizeable: 'false',
                     filterable: 'true',
-                    filterMethod: (filter, row) =>
-                        String(row[filter.id]).toLowerCase().includes(String(filter.value).toLowerCase()),
+                    filterAll: 'true',
                     headerStyle: {textAlign: 'left', borderColor: 'black', borderStyle: 'none none outset none', borderWidth: '2px',},
                     style: {textAlign: 'left',borderColor: 'lightgrey', borderStyle: 'none none outset none',borderWidth: '1px',} 
                 }, {
@@ -92,8 +107,7 @@ class Results extends React.Component {
                     minWidth: 100,
                     resizeable: 'false',
                     filterable: 'true',
-                    filterMethod: (filter, row) =>
-                        String(row[filter.id]).toLowerCase().includes(String(filter.value).toLowerCase()),
+                    filterAll: 'true',
                     headerStyle: {textAlign: 'left', borderColor: 'black', borderStyle: 'none none outset none', borderWidth: '2px',},
                     style: {textAlign: 'left',borderColor: 'lightgrey', borderStyle: 'none none outset none', borderWidth: '1px',}
                 }, {
@@ -102,18 +116,42 @@ class Results extends React.Component {
                     minWidth: 75,
                     resizeable: 'false',
                     filterable: 'true',
-                    filterMethod: (filter, row) => {
+                    filterAll: 'true',
+                    filterMethod: (filter, arr) => {
+                        let resultArray = [{name: "Filtered Total Hours", email: "n/a", lastDate: "n/a", totalHours: 0}];
                         let comp = String(filter.value).charAt(0);
-                        if(comp === '>'){
-                        console.log(parseInt(String(filter.value).substr(1,),10))
-                        return row[filter.id] > parseInt(String(filter.value).substr(1,), 10);
-                        }
-                        else if(comp === '<'){
-                        return row[filter.id] < parseInt(String(filter.value).substr(1,),10);
-                        }
-                        else{
-                        return row[filter.id] === parseInt(String(filter.value),10);
-                        }
+                        let num = parseInt(String(filter.value).substr(1,), 10)
+                        if(Number.isInteger(num) && !(String(filter.value).length > 1)){
+                            resultArray = this.props.displayScreen ? this.state.data : this.state.data2;
+                        } else {
+                            for(var i = 0; i<arr.length; i++){
+                                if(arr[i]['name'] === "Filtered Total Hours"){
+                                    continue;
+                                }
+                                if(comp === '>') {
+                                    if(arr[i][filter.id] > parseInt(String(filter.value).substr(1,), 10)) {
+                                        resultArray.push(arr[i]);
+                                    }
+                                }
+                                else if(comp === '<') {
+                                    if(arr[i][filter.id] < parseInt(String(filter.value).substr(1,),10)) {
+                                        resultArray.push(arr[i]);
+                                    }
+                                }
+                                else {
+                                    //console.log(arr[i][filter.id])
+                                    if(arr[i][filter.id] === parseInt(String(filter.value),10)){
+                                        resultArray.push(arr[i]);
+                                    }
+                                }
+                            }
+                            let filterTotal = 0;
+                            for(let j = 0; j < resultArray.length; j++){
+                                filterTotal += resultArray[j]['totalHours'];
+                            }
+                            resultArray[0]['totalHours'] = filterTotal;
+                        }                        
+                        return resultArray;
                     },
                     headerStyle: {textAlign: 'center', borderColor: 'black', borderStyle: 'none none outset none', borderWidth: '2px', minWidth: '20px', marginRight: '10px'},
                     style: {fontWeight: 'bold', textAlign: 'center', borderColor: 'lightgrey', borderStyle: 'none none outset none', borderWidth: '1px', marginRight: '10px'}
@@ -131,8 +169,9 @@ class Results extends React.Component {
                         accessor: 'date',
                         minWidth: 100,
                         filterable: 'true',
-                        filterMethod: (filter, row) =>
-                            String(row[filter.id]).toLowerCase().includes(String(filter.value).toLowerCase()),
+                        filterAll: 'true',
+                        // filterMethod: (filter, row) =>
+                        //     String(row[filter.id]).toLowerCase().includes(String(filter.value).toLowerCase()),
                         headerStyle: {textAlign: 'left', borderColor: 'black', borderStyle: 'none none outset none', borderWidth: '2px', marginLeft: '10px',},
                         style: {textAlign: 'left',borderColor: 'lightgrey', borderStyle: 'none none outset none',borderWidth: '1px', marginLeft: '10px',}
                     }, {
@@ -140,8 +179,9 @@ class Results extends React.Component {
                         accessor: 'name',
                         minWidth: 300,
                         filterable: 'true',
-                        filterMethod: (filter, row) =>
-                            String(row[filter.id]).toLowerCase().includes(String(filter.value).toLowerCase()),
+                        filterAll: 'true',
+                        // filterMethod: (filter, row) =>
+                        //     String(row[filter.id]).toLowerCase().includes(String(filter.value).toLowerCase()),
                         headerStyle: {textAlign: 'left', borderColor: 'black', borderStyle: 'none none outset none', borderWidth: '2px',},
                         style: {textAlign: 'left',borderColor: 'lightgrey', borderStyle: 'none none outset none',borderWidth: '1px',}                       
                     }, {
@@ -149,8 +189,9 @@ class Results extends React.Component {
                         accessor: 'email',
                         minWidth: 400,
                         filterable: 'true',
-                        filterMethod: (filter, row) =>
-                            String(row[filter.id]).toLowerCase().includes(String(filter.value).toLowerCase()),
+                        filterAll: 'true',
+                        // filterMethod: (filter, row) =>
+                        //     String(row[filter.id]).toLowerCase().includes(String(filter.value).toLowerCase()),
                         headerStyle: {textAlign: 'left', borderColor: 'black', borderStyle: 'none none outset none', borderWidth: '2px'},
                         style: {textAlign: 'left', borderColor: 'lightgrey', borderStyle: 'none none outset none', borderWidth: '1px'}
                     }, {
@@ -158,18 +199,44 @@ class Results extends React.Component {
                         accessor: 'hours',
                         minWidth: 100,
                         filterable: 'true',
-                        filterMethod: (filter, row) => {
-                        let comp = String(filter.value).charAt(0);
-                            if(comp === '>'){
-                            console.log(parseInt(String(filter.value).substr(1,),10))
-                            return row[filter.id] > parseInt(String(filter.value).substr(1,), 10);
+                        filterAll: 'true',
+                        filterMethod: (filter, arr) => {
+                            let resultArray = [{name: "Filtered Total Hours", email: "n/a", date: "n/a", hours: 0}];
+                            let comp = String(filter.value).charAt(0);
+                            let num = parseInt(String(filter.value).substr(1,), 10)
+                            if(Number.isInteger(num) && !(String(filter.value).length > 1)){
+                                resultArray = this.props.displayScreen ? this.state.data : this.state.data2;
+                            } else {
+                                for(let i=0; i < arr.length; i++){
+                                    if(arr[i]['name'] === "Filtered Total Hours"){
+                                        continue;
+                                    }
+                                    if(comp === '>'){
+                                    console.log(parseInt(String(filter.value).substr(1,),10))
+                                        if(arr[i][filter.id] > parseInt(String(filter.value).substr(1,), 10)){
+                                            resultArray.push(arr[i]);
+                                        }
+                                    }
+                                    else if(comp === '<'){
+                                        if(arr[i][filter.id] < parseInt(String(filter.value).substr(1,),10)){
+                                            resultArray.push(arr[i]);
+                                        }
+                                    }
+                                    else{
+                                        if(arr[i][filter.id] === parseInt(String(filter.value),10)){
+                                            resultArray.push(arr[i]);   
+                                        }
+                                    }
+                                }
+                                let resultSum = 0;
+                                for(let j=0; j<resultArray.length; j++){
+                                    resultSum += resultArray[j]['hours'];
+                                }
+                                console.log(resultSum)
+                                resultArray[0]['hours'] = resultSum; 
                             }
-                            else if(comp === '<'){
-                            return row[filter.id] < parseInt(String(filter.value).substr(1,),10);
-                            }
-                            else{
-                            return row[filter.id] === parseInt(String(filter.value),10);
-                            }
+                            return resultArray;
+
                         },
                         headerStyle: {textAlign: 'center', borderColor: 'black', borderStyle: 'none none outset none', borderWidth: '2px'},
                         style: {alignItems: 'center', borderColor: 'lightgrey', borderStyle: 'none none outset none', borderWidth: '1px', fontWeight: 'bold'}                        
@@ -180,13 +247,13 @@ class Results extends React.Component {
                     minWidth: 50,
                     headerStyle: {textAlign: 'center', borderColor: 'black', borderStyle: 'none none outset none', borderWidth: '2px', marginRight: '10px'},
                     style: {alignItems: 'center', borderColor: 'lightgrey', borderStyle: 'none none outset none', borderWidth: '1px', marginRight: '10px'},
-                    Cell: ({ value }) => <div className = "Delete-icon"><MdRemoveCircle style={{width: '24px',height: '24px'}} tabIndex={0}/></div>
+                    Cell: ({ value }) => value === 'n/a' ? null : <div className = "Delete-icon"><MdRemoveCircle style={{width: '24px',height: '24px'}} tabIndex={0}/></div>
                 }
                 ],
                 style: {textAlign: 'left'}
             }
         ];
-        let tableData = this.props.displayScreen ? this.state.data : this.state.data2;
+        let tableData = this.props.displayScreen ? this.state.displayData1 : this.state.displayData2;
         let tableColumns = this.props.displayScreen ? columns : columns2;
         return(
             <div> 
@@ -199,6 +266,24 @@ class Results extends React.Component {
                     data={tableData}
                     columns={tableColumns}
                     defaultPageSize={10}
+                    defaultFilterMethod={ (filter, arr) =>{
+                        let resultArray = [{name: "Filtered Total Hours", email: "n/a", lastDate: "n/a", totalHours: 0, date: 'n/a', hours: 0, delete: 'n/a'}]
+                        for(let i=0; i< arr.length; i++){
+                            if (String(arr[i][filter.id]).toLowerCase().includes(String(filter.value).toLowerCase())){
+                                resultArray.push(arr[i]);
+                            }
+                        }
+                        
+                        let resultSumTH = 0;
+                        let resultSumH = 0;
+                        for(let j=0; j<resultArray.length; j++){
+                            resultSumTH += resultArray[j]['totalHours'];
+                            resultSumH += resultArray[j]['hours']
+                        }
+                        resultArray[0]['totalHours'] = resultSumTH;
+                        resultArray[0]['hours'] = resultSumH;
+                        return resultArray;
+                    }}
                     className="-highlight"
                     getTdProps={(state,rowInfo,column, instance) => {
                         return{
